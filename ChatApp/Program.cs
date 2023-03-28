a50e1e88d5030ef7b77fb4d2562f0a82bdf6225d
shalini-lodhi <shalinilodhi01@gmail.com>
2023-03-28 10:33:50 +0530

    Add project files.

---
+++ ChatApp/Program.cs
@@ -0,0 +1,42 @@
+using ChatApp.Data;
+using ChatApp.Hubs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<WeatherForecastService>();
+        //compressing the messages
+        builder.Services.AddResponseCompression(opts =>
+        {
+            opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapHub<ChatHub>("/chathub");
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
+}
